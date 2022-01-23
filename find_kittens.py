38da2510fb34ad17ac1b42a242ebc126f2b01ac5
Devin Cunningham <devin.cunningham@sjsu.edu>
2022-01-22 20:39:34 -0800

    added some lazy exception handling

--- find_kittens.py
+++ find_kittens.py
@@ -10,56 +10,75 @@ from kitten_finder import KittenFinder
 
 
 if __name__ == "__main__":
+    # Load in our secret environment variables.
     load_dotenv()
+
+    # Set up my twilio credentials.
     twilio_account_sid = os.environ['TWILIO_ACCOUNT_SID']
     twilio_auth_token = os.environ['TWILIO_AUTH_TOKEN']
     twilio_client = Client(twilio_account_sid, twilio_auth_token)
+
+    # Initialize our kitten finder class.
     kf = KittenFinder()
 
-    while True:
-        # Scrape petfinder.
-        available_kittens_df = kf.fetch_data()
+    try:
+        while True:
+            # Scrape petfinder.
+            available_kittens_df = kf.fetch_data()
 
-        # Read in the previously scraped data.
-        historical_kittens_df = pd.read_csv('historical_kittens.csv', index_col='id')
+            # Read in the previously scraped data.
+            historical_kittens_df = pd.read_csv('historical_kittens.csv', index_col='id')
 
-        # Compare the newly scraped data with the previously scraped data.
-        merged_df = available_kittens_df.merge(historical_kittens_df, on='id', how='outer', indicator=True)
-        new_kitten_ids = merged_df[merged_df['_merge'] == 'left_only'].index
-        new_kittens_df = available_kittens_df.loc[new_kitten_ids]
+            # Compare the newly scraped data with the previously scraped data.
+            merged_df = available_kittens_df.merge(historical_kittens_df, on='id', how='outer', indicator=True)
+            new_kitten_ids = merged_df[merged_df['_merge'] == 'left_only'].index
+            new_kittens_df = available_kittens_df.loc[new_kitten_ids]
 
-        # Overwrite the old csv with the newly scraped data. Out with the old, in with the new!
-        available_kittens_df.to_csv('historical_kittens.csv', index=True)
+            # Overwrite the old csv with the newly scraped data. Out with the old, in with the new!
+            available_kittens_df.to_csv('historical_kittens.csv', index=True)
 
-        # If there were any new kittens, text me!
-        if not new_kittens_df.empty:
-            for _, kitten in new_kittens_df.iterrows():
+            # If there were any new kittens, text me!
+            if not new_kittens_df.empty:
+                for _, kitten in new_kittens_df.iterrows():
 
-                # Craft a message.
-                if kitten['breeds']['secondary']:
-                    kitten_breed = f"{kitten['breeds']['primary']}/{kitten['breeds']['secondary']}".lower()
-                else:
-                    kitten_breed = kitten['breeds']['primary'].lower()
-                kitten_gender = kitten['gender'].lower()
-                kitten_name = kitten['name']
-                if kitten['primary_photo_cropped']:
-                    kitten_photo = kitten['primary_photo_cropped']['full']
-                else:
-                    kitten_photo = values.unset
-                kitten_url = kitten['url']
-                message = "New kitten available for adoption: " \
-                          f"a {kitten_gender} {kitten_breed} named {kitten_name}! \n{kitten_url}"
+                    # Craft a message.
+                    if kitten['breeds']['secondary']:
+                        kitten_breed = f"{kitten['breeds']['primary']}/{kitten['breeds']['secondary']}".lower()
+                    else:
+                        kitten_breed = kitten['breeds']['primary'].lower()
+                    kitten_gender = kitten['gender'].lower()
+                    kitten_name = kitten['name']
+                    if kitten['primary_photo_cropped']:
+                        kitten_photo = kitten['primary_photo_cropped']['full']
+                    else:
+                        kitten_photo = values.unset
+                    kitten_url = kitten['url']
+                    message = "New kitten available for adoption: " \
+                              f"a {kitten_gender} {kitten_breed} named {kitten_name}! \n{kitten_url}"
 
-                print(message)
-                # Text the message to my phone.
-                twilio_client.messages.create(
-                    body=message,
-                    from_=os.environ['MY_TWILIO_NUMBER'],
-                    media_url=kitten_photo,
-                    to=os.environ['MY_CELL_NUMBER'])
+                    print(message)
+                    # Text the message to my phone.
+                    twilio_client.messages.create(
+                        body=message,
+                        from_=os.environ['MY_TWILIO_NUMBER'],
+                        media_url=kitten_photo,
+                        to=os.environ['MY_CELL_NUMBER'])
 
-                # A short sleep to protect against my Twilio trial data limits.
-                time.sleep(5)
+                    # A short sleep to protect against my Twilio trial data limits.
+                    time.sleep(5)
+            else:
+                print("No new kittens yet! Try again later.")
 
-        # After all is said and done, take a quick break and then try it again!
-        time.sleep(60)
+            # After all is said and done, take a quick break and then try it again!
+            time.sleep(60)
+
+    # If anything goes wrong, try to text me and then end the script.
+    # I know that wrapping everything in one giant try/except is bad,
+    # but this is a simple script for a simple task!
+    except Exception:
+        twilio_client.messages.create(
+            body="The kitten finder got all tangled up! Come help!",
+            from_=os.environ['MY_TWILIO_NUMBER'],
+            to=os.environ['MY_CELL_NUMBER'],
+        )
+        raise
